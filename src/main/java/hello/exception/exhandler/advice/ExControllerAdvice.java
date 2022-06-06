84783ef59285524098666ad618a12667f3b036a9
4books <joonwoo9192@gmail.com>
2022-06-07 00:47:06 +0900

    Error Handling

---
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,38 @@
+package hello.exception.exhandler.advice;
+
+import hello.exception.api.ApiExceptionV2Controller;
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+//@RestControllerAdvice(assignableTypes = ApiExceptionV2Controller.class)
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler //argument 와 같다면 생략해도 됨
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    //공통 처리 또는 해결하지 못한 에러는 Exception 인 자식들을 전부 커버해서 여기로 온다.
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+}
